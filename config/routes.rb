4c7bb1ae78dbb8cb6dec3d5fd6b48e3bcfc5aa35
zarkuslol <lucasferlima8@gmail.com>
2023-08-02 23:13:14 -0300

    User scaffold, create, migrate and first rule created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users
+  root 'users#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/user_phone', to: "users#get_by_phone"
 end
